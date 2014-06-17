a4ac385c1ad5ae14d337fc1081c644638b9a5391
Timothy Bish <tabish121@gmail.com>
2014-06-17 12:06:24 -0400

    More tests for the openwire code, move some bits around.

--- openwire-core/src/test/java/io/openwire/ide/OpenWireCodecGenerator.java
+++ openwire-core/src/test/java/io/openwire/ide/OpenWireCodecGenerator.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.openwire.util;
+package io.openwire.ide;
 
 import io.openwire.generator.GeneratorTask;
 
