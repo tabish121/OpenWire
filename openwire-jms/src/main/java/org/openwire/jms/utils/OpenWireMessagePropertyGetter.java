17135d6949fa632b9a46c87f56e6a972ff841625
Timothy Bish <tabish121@gmail.com>
2014-06-19 19:18:09 -0400

    Add utility classes to intercept calls to property set and get and map them to proper OpenWireMessage fields as needed.

---
+++ openwire-jms/src/main/java/org/openwire/jms/utils/OpenWireMessagePropertyGetter.java
@@ -0,0 +1,253 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openwire.jms.utils;
+
+import io.openwire.commands.OpenWireMessage;
+import io.openwire.commands.TransactionId;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.jms.Destination;
+import javax.jms.JMSException;
+
+/**
+ * Utility class used to intercept calls to Message property gets and map the
+ * correct OpenWire fields to the property name being queried.
+ */
+public class OpenWireMessagePropertyGetter {
+
+    private static final Map<String, PropertyGetter> PROPERTY_GETTERS = new HashMap<String, PropertyGetter>();
+
+    interface PropertyGetter {
+        Object getProperty(OpenWireMessage message);
+    }
+
+    static {
+        PROPERTY_GETTERS.put("JMSDestination", new PropertyGetter() {
+            @Override
+            public Object getProperty(OpenWireMessage message) {
+                Destination dest = message.getOriginalDestination();
+                if (dest == null) {
+                    dest = message.getDestination();
+                }
+                if (dest == null) {
+                    return null;
+                }
+                return dest.toString();
+            }
+        });
+        PROPERTY_GETTERS.put("JMSReplyTo", new PropertyGetter() {
+            @Override
+            public Object getProperty(OpenWireMessage message) {
+                if (message.getReplyTo() == null) {
+                    return null;
+                }
+                return message.getReplyTo().toString();
+            }
+        });
+        PROPERTY_GETTERS.put("JMSType", new PropertyGetter() {
+            @Override
+            public Object getProperty(OpenWireMessage message) {
+                return message.getType();
+            }
+        });
+        PROPERTY_GETTERS.put("JMSDeliveryMode", new PropertyGetter() {
+            @Override
+            public Object getProperty(OpenWireMessage message) {
+                return message.isPersistent() ? "PERSISTENT" : "NON_PERSISTENT";
+            }
+        });
+        PROPERTY_GETTERS.put("JMSPriority", new PropertyGetter() {
+            @Override
+            public Object getProperty(OpenWireMessage message) {
+                return Integer.valueOf(message.getPriority());
+            }
+        });
+        PROPERTY_GETTERS.put("JMSMessageID", new PropertyGetter() {
+            @Override
+            public Object getProperty(OpenWireMessage message) {
+                if (message.getMessageId() == null) {
+                    return null;
+                }
+                return message.getMessageId().toString();
+            }
+        });
+        PROPERTY_GETTERS.put("JMSTimestamp", new PropertyGetter() {
+            @Override
+            public Object getProperty(OpenWireMessage message) {
+                return Long.valueOf(message.getTimestamp());
+            }
+        });
+        PROPERTY_GETTERS.put("JMSCorrelationID", new PropertyGetter() {
+            @Override
+            public Object getProperty(OpenWireMessage message) {
+                return message.getCorrelationId();
+            }
+        });
+        PROPERTY_GETTERS.put("JMSExpiration", new PropertyGetter() {
+            @Override
+            public Object getProperty(OpenWireMessage message) {
+                return Long.valueOf(message.getExpiration());
+            }
+        });
+        PROPERTY_GETTERS.put("JMSRedelivered", new PropertyGetter() {
+            @Override
+            public Object getProperty(OpenWireMessage message) {
+                return Boolean.valueOf(message.isRedelivered());
+            }
+        });
+        PROPERTY_GETTERS.put("JMSXDeliveryCount", new PropertyGetter() {
+            @Override
+            public Object getProperty(OpenWireMessage message) {
+                return Integer.valueOf(message.getRedeliveryCounter() + 1);
+            }
+        });
+        PROPERTY_GETTERS.put("JMSXGroupID", new PropertyGetter() {
+            @Override
+            public Object getProperty(OpenWireMessage message) {
+                return message.getGroupId();
+            }
+        });
+        PROPERTY_GETTERS.put("JMSXUserID", new PropertyGetter() {
+            @Override
+            public Object getProperty(OpenWireMessage message) {
+                Object userId = message.getUserId();
+                if (userId == null) {
+                    try {
+                        userId = message.getProperty("JMSXUserID");
+                    } catch (JMSException e) {
+                    }
+                }
+
+                return userId;
+            }
+        });
+        PROPERTY_GETTERS.put("JMSXGroupSeq", new PropertyGetter() {
+            @Override
+            public Object getProperty(OpenWireMessage message) {
+                return new Integer(message.getGroupSequence());
+            }
+        });
+        PROPERTY_GETTERS.put("JMSXProducerTXID", new PropertyGetter() {
+            @Override
+            public Object getProperty(OpenWireMessage message) {
+                TransactionId txId = message.getOriginalTransactionId();
+                if (txId == null) {
+                    txId = message.getTransactionId();
+                }
+                if (txId == null) {
+                    return null;
+                }
+                return txId.toString();
+            }
+        });
+        PROPERTY_GETTERS.put("JMSActiveMQBrokerInTime", new PropertyGetter() {
+            @Override
+            public Object getProperty(OpenWireMessage message) {
+                return Long.valueOf(message.getBrokerInTime());
+            }
+        });
+        PROPERTY_GETTERS.put("JMSActiveMQBrokerOutTime", new PropertyGetter() {
+            @Override
+            public Object getProperty(OpenWireMessage message) {
+                return Long.valueOf(message.getBrokerOutTime());
+            }
+        });
+        PROPERTY_GETTERS.put("JMSActiveMQBrokerPath", new PropertyGetter() {
+            @Override
+            public Object getProperty(OpenWireMessage message) {
+                return Arrays.toString(message.getBrokerPath());
+            }
+        });
+        PROPERTY_GETTERS.put("JMSXGroupFirstForConsumer", new PropertyGetter() {
+            @Override
+            public Object getProperty(OpenWireMessage message) {
+                return Boolean.valueOf(message.isJMSXGroupFirstForConsumer());
+            }
+        });
+    }
+
+    private final String name;
+    private final PropertyGetter jmsPropertyExpression;
+
+    /**
+     * Creates an new property getter instance that is assigned to read the named value.
+     *
+     * @param name
+     *        the property value that this getter is assigned to lookup.
+     */
+    public OpenWireMessagePropertyGetter(String name) {
+        this.name = name;
+        jmsPropertyExpression = PROPERTY_GETTERS.get(name);
+    }
+
+    /**
+     * Gets the correct property value from the OpenWireMessage instance based on
+     * the predefined property mappings.
+     *
+     * @param message
+     *        the OpenWireMessage whose property is being read.
+     *
+     * @return the correct value either mapped to an OpenWire attribute of a Message property.
+     *
+     * @throws JMSException if an error occurs while reading the defined property.
+     */
+    public Object get(OpenWireMessage message) throws JMSException {
+        if (jmsPropertyExpression != null) {
+            return jmsPropertyExpression.getProperty(message);
+        }
+
+        return message.getProperty(name);
+    }
+
+    /**
+     * @return the property name that is being intercepted for the OpenWireMessage.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    /**
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+
+    /**
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object o) {
+
+        if (o == null || !this.getClass().equals(o.getClass())) {
+            return false;
+        }
+        return name.equals(((OpenWireMessagePropertyGetter) o).name);
+    }
+}
---
+++ openwire-jms/src/main/java/org/openwire/jms/utils/OpenWireMessagePropertySetter.java
@@ -0,0 +1,226 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openwire.jms.utils;
+
+import io.openwire.commands.OpenWireDestination;
+import io.openwire.commands.OpenWireMessage;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.jms.DeliveryMode;
+import javax.jms.JMSException;
+import javax.jms.MessageFormatException;
+
+/**
+ * Utility class used to intercept calls to Message property sets and map the
+ * correct OpenWire fields to the property name being set.
+ */
+public class OpenWireMessagePropertySetter {
+
+    private static final Map<String, PropertySetter> PROPERTY_SETTERS = new HashMap<String, PropertySetter>();
+
+    interface PropertySetter {
+        void setProperty(OpenWireMessage message, Object value) throws MessageFormatException;
+    }
+
+    static {
+        PROPERTY_SETTERS.put("JMSXDeliveryCount", new PropertySetter() {
+            @Override
+            public void setProperty(OpenWireMessage message, Object value) throws MessageFormatException {
+                Integer rc = (Integer) TypeConversionSupport.convert(value, Integer.class);
+                if (rc == null) {
+                    throw new MessageFormatException("Property JMSXDeliveryCount cannot be set from a " + value.getClass().getName() + ".");
+                }
+                message.setRedeliveryCounter(rc.intValue() - 1);
+            }
+        });
+        PROPERTY_SETTERS.put("JMSXGroupID", new PropertySetter() {
+            @Override
+            public void setProperty(OpenWireMessage message, Object value) throws MessageFormatException {
+                String rc = (String) TypeConversionSupport.convert(value, String.class);
+                if (rc == null) {
+                    throw new MessageFormatException("Property JMSXGroupID cannot be set from a " + value.getClass().getName() + ".");
+                }
+                message.setGroupID(rc);
+            }
+        });
+        PROPERTY_SETTERS.put("JMSXGroupSeq", new PropertySetter() {
+            @Override
+            public void setProperty(OpenWireMessage message, Object value) throws MessageFormatException {
+                Integer rc = (Integer) TypeConversionSupport.convert(value, Integer.class);
+                if (rc == null) {
+                    throw new MessageFormatException("Property JMSXGroupSeq cannot be set from a " + value.getClass().getName() + ".");
+                }
+                message.setGroupSequence(rc.intValue());
+            }
+        });
+        PROPERTY_SETTERS.put("JMSCorrelationID", new PropertySetter() {
+            @Override
+            public void setProperty(OpenWireMessage message, Object value) throws MessageFormatException {
+                String rc = (String) TypeConversionSupport.convert(value, String.class);
+                if (rc == null) {
+                    throw new MessageFormatException("Property JMSCorrelationID cannot be set from a " + value.getClass().getName() + ".");
+                }
+                message.setCorrelationId(rc);
+            }
+        });
+        PROPERTY_SETTERS.put("JMSDeliveryMode", new PropertySetter() {
+            @Override
+            public void setProperty(OpenWireMessage message, Object value) throws MessageFormatException {
+                Integer rc = (Integer) TypeConversionSupport.convert(value, Integer.class);
+                if (rc == null) {
+                    Boolean bool = (Boolean) TypeConversionSupport.convert(value, Boolean.class);
+                    if (bool == null) {
+                        throw new MessageFormatException("Property JMSDeliveryMode cannot be set from a " + value.getClass().getName() + ".");
+                    } else {
+                        message.setPersistent(bool.booleanValue());
+                    }
+                } else {
+                    message.setPersistent(rc == DeliveryMode.PERSISTENT);
+                }
+            }
+        });
+        PROPERTY_SETTERS.put("JMSExpiration", new PropertySetter() {
+            @Override
+            public void setProperty(OpenWireMessage message, Object value) throws MessageFormatException {
+                Long rc = (Long) TypeConversionSupport.convert(value, Long.class);
+                if (rc == null) {
+                    throw new MessageFormatException("Property JMSExpiration cannot be set from a " + value.getClass().getName() + ".");
+                }
+                message.setExpiration(rc.longValue());
+            }
+        });
+        PROPERTY_SETTERS.put("JMSPriority", new PropertySetter() {
+            @Override
+            public void setProperty(OpenWireMessage message, Object value) throws MessageFormatException {
+                Integer rc = (Integer) TypeConversionSupport.convert(value, Integer.class);
+                if (rc == null) {
+                    throw new MessageFormatException("Property JMSPriority cannot be set from a " + value.getClass().getName() + ".");
+                }
+                message.setPriority(rc.byteValue());
+            }
+        });
+        PROPERTY_SETTERS.put("JMSRedelivered", new PropertySetter() {
+            @Override
+            public void setProperty(OpenWireMessage message, Object value) throws MessageFormatException {
+                Boolean rc = (Boolean) TypeConversionSupport.convert(value, Boolean.class);
+                if (rc == null) {
+                    throw new MessageFormatException("Property JMSRedelivered cannot be set from a " + value.getClass().getName() + ".");
+                }
+                message.setRedelivered(rc.booleanValue());
+            }
+        });
+        PROPERTY_SETTERS.put("JMSReplyTo", new PropertySetter() {
+            @Override
+            public void setProperty(OpenWireMessage message, Object value) throws MessageFormatException {
+                OpenWireDestination rc = (OpenWireDestination) TypeConversionSupport.convert(value, OpenWireDestination.class);
+                if (rc == null) {
+                    throw new MessageFormatException("Property JMSReplyTo cannot be set from a " + value.getClass().getName() + ".");
+                }
+                message.setReplyTo(rc);
+            }
+        });
+        PROPERTY_SETTERS.put("JMSTimestamp", new PropertySetter() {
+            @Override
+            public void setProperty(OpenWireMessage message, Object value) throws MessageFormatException {
+                Long rc = (Long) TypeConversionSupport.convert(value, Long.class);
+                if (rc == null) {
+                    throw new MessageFormatException("Property JMSTimestamp cannot be set from a " + value.getClass().getName() + ".");
+                }
+                message.setTimestamp(rc.longValue());
+            }
+        });
+        PROPERTY_SETTERS.put("JMSType", new PropertySetter() {
+            @Override
+            public void setProperty(OpenWireMessage message, Object value) throws MessageFormatException {
+                String rc = (String) TypeConversionSupport.convert(value, String.class);
+                if (rc == null) {
+                    throw new MessageFormatException("Property JMSType cannot be set from a " + value.getClass().getName() + ".");
+                }
+                message.setType(rc);
+            }
+        });
+    }
+
+    private final String name;
+    private final PropertySetter jmsPropertyExpression;
+
+    /**
+     * Creates an new property getter instance that is assigned to read the named value.
+     *
+     * @param name
+     *        the property value that this getter is assigned to lookup.
+     */
+    public OpenWireMessagePropertySetter(String name) {
+        this.name = name;
+        jmsPropertyExpression = PROPERTY_SETTERS.get(name);
+    }
+
+    /**
+     * Sets the correct property value from the OpenWireMessage instance based on
+     * the predefined property mappings.
+     *
+     * @param message
+     *        the OpenWireMessage whose property is being read.
+     * @param value
+     *        the value to be set on the intercepted OpenWireMessage property.
+     *
+     * @throws JMSException if an error occurs while reading the defined property.
+     */
+    public void set(OpenWireMessage message, Object value) throws JMSException {
+        if (jmsPropertyExpression != null) {
+            jmsPropertyExpression.setProperty(message, value);
+        }
+
+        message.setProperty(name, value);
+    }
+
+    /**
+     * @return the property name that is being intercepted for the OpenWireMessage.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    /**
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+
+    /**
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || !this.getClass().equals(o.getClass())) {
+            return false;
+        }
+        return name.equals(((OpenWireMessagePropertySetter) o).name);
+    }
+}
